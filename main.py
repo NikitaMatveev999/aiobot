6b92f7bcb88f258a06fff074e5bf6ebe8ae81833
NikitaMatveev999 <NikitaMatveev999@outlook.com>
2023-07-05 13:01:54 +0300

    add send cat images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import requests
 from dotenv import load_dotenv, find_dotenv
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -12,6 +13,7 @@ bot = Bot(token=os.getenv('TOKEN'))
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 my_chat_id = os.getenv('ID')
+cat_api_url = os.getenv('CAT_API')
 
 
 @dp.message_handler(commands=['start'])
@@ -34,6 +36,13 @@ async def handle_message(message: types.Message):
     forward_message += f'Пользователь: {user_first_name} {user_last_name} (@{user_username})\n'
     forward_message += f'ID пользователя: {user_id}\n'
     forward_message += f'Сообщение: {text}'
+
+    response = requests.get(cat_api_url)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            cat_image_url = data[0]['url']
+            await bot.send_photo(chat_id=message.chat.id, photo=cat_image_url)
 
     await bot.send_message(chat_id=my_chat_id, text=forward_message)
 
